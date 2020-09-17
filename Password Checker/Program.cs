2b6de3c9bcfe7b9fce5a641f31cda8886acd31b2
Gaven Rudiak <gavenrudiak@outlook.com>
2020-09-17 08:21:20 -0400

    Add project files.

---
+++ Password Checker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Password_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 1;
+            int minLength = 8;
+            string uppercase, lowercase, digits, specialChars;
+
+            Console.WriteLine($"Hello there please enter your password. {score}");
+            string password = Console.ReadLine(); 
+
+            if (password.Length >= minLength)
+            {
+                score++;
+            }
+            
+
+        }
+    }
+}
